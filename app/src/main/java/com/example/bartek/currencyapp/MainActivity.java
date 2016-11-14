1f164d4a5580e665693fc81943ac1d8e8875c80f
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2016-11-14 20:24:11 +0100

    Use Action-Bar for menu bar.

--- app/src/main/java/com/example/bartek/currencyapp/MainActivity.java
+++ app/src/main/java/com/example/bartek/currencyapp/MainActivity.java
@@ -11,11 +11,14 @@ import org.apache.http.util.EntityUtils;
 import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -50,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu1, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void fillCurrencyList() {
 
