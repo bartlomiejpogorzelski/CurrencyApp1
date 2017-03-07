ddff1a5004954aa21dc7328a503ab4dab3d416f8
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-03-07 11:41:11 +0100

    Update currency
    Now it is possible to buy currency and update list: accountsList in
    AccountsActivity.
    I see buy-value currency in accountsList.

--- app/src/main/java/com/example/bartek/currencyapp/AccountsActivity.java
+++ app/src/main/java/com/example/bartek/currencyapp/AccountsActivity.java
@@ -15,12 +15,11 @@ import java.util.ArrayList;
 public class AccountsActivity extends AppCompatActivity {
 
     public static Account mainAccount= new Account(13000.0, new Currency(1,1,"PLN"));
+    public static ArrayList<Account> accountsList = new ArrayList<Account>();
 
-    CurrencyAdapter.HolderView holder= new CurrencyAdapter.HolderView();
-
-    Button buttonSmall;
     TextView textView;
     ListView listb;
+    TextView otherAmount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,30 +27,26 @@ public class AccountsActivity extends AppCompatActivity {
 
         textView= (TextView) findViewById(R.id.textAmount);
         listb= (ListView) findViewById(R.id.listB);
-
         textView.setText(String.valueOf(mainAccount.getAmount()+"  zł"));
 
-        ArrayList<Account> accountsList = new ArrayList<Account>();
-        AccountsAdapter aa = new AccountsAdapter(getApplicationContext(), R.layout.row1, accountsList);
-
-        for (int i = 0; i < MainActivity.currencyArrayList.size() ; i++) {
-            accountsList.add(new Account(1000.0, MainActivity.currencyArrayList.get(i)));
+        AccountsAdapter accountsAdapter = new AccountsAdapter(getApplicationContext(), R.layout.row1, accountsList);
+        for (int i = 0; i < MainActivity.currencyArrayList.size() ; i++)
+        {
+            accountsList.add(new Account(0.0, MainActivity.currencyArrayList.get(i)));
         }
 
-        listb.setAdapter(aa);
-        aa.notifyDataSetChanged();
+        otherAmount = (TextView) findViewById(R.id.amountOtherCurrency);
+        listb.setAdapter(accountsAdapter);
+        accountsAdapter.notifyDataSetChanged();
 //        listb.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 //                Toast.makeText(getApplicationContext(), "dziala", Toast.LENGTH_SHORT).show();
 //                Intent anotherIntent= new Intent(getApplicationContext(), ExchangeActivity.class);
-//
 //               startActivity(anotherIntent);
 //            }
 //        });
-
 //        buttonSmall= (Button) findViewById(R.id.buttonSmall);
-//
 //        buttonSmall.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View view) {
@@ -59,17 +54,12 @@ public class AccountsActivity extends AppCompatActivity {
 //            }
 //        });
     }
-
     public void btnAllCurrencies(View view) {
         Intent anotherIntent= new Intent(getApplicationContext(), ExchangeActivity.class);
         startActivity(anotherIntent);
-
     }
-
 //      public void BtnSmall(View view) {
-//
 //         Intent anotherIntent1= new Intent(getApplicationContext(), ExchangeActivity.class);
 //                  startActivity(anotherIntent1);
 //       }
-
 }
--- app/src/main/java/com/example/bartek/currencyapp/ExchangeActivity.java
+++ app/src/main/java/com/example/bartek/currencyapp/ExchangeActivity.java
@@ -27,42 +27,34 @@ public class ExchangeActivity extends AppCompatActivity implements AdapterView.O
     EditText editText;
     Button buyButton;
     double foreignCurs;
-    static double buyAmount;
+    double buyAmount ;
     double plnAmount;
     TextView otherAmount;
-    ArrayList<Account> t= new ArrayList<>() ;
+    double result;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
 
-
         textAmount=(TextView)findViewById(R.id.textAmount1);
-
         textForeign=(TextView)findViewById(R.id.textForeign);
-
         editText=(EditText) findViewById(R.id.EditSum);
         buyButton=(Button)findViewById(R.id.BuyButton);
-
         textAmount.setText(mainAccount.getAmount().toString()); //it can be String.valueof()
-
         textForeign.setText(String.valueOf("Course of currency "));
-
         addSpinnerList();
         otherAmount = (TextView) findViewById(R.id.amountOtherCurrency);
+
     }
-
     private void addSpinnerList() {
-         Spinner spinner = (Spinner) findViewById(R.id.spinner);
-
+        spinner = (Spinner) findViewById(R.id.spinner);
         String[] array_spinner;
         ArrayList<String> list1= new ArrayList();
-
         for (int y=0; y < MainActivity.currencyArrayList.size() ; y++)
         {
             array_spinner= new String[MainActivity.currencyArrayList.size()];
             array_spinner[y]=String.valueOf(MainActivity.currencyArrayList.get(y).getSymbol());
-
             for (String s : array_spinner)
             {
                 if (s != null && s.length() >0)
@@ -71,54 +63,50 @@ public class ExchangeActivity extends AppCompatActivity implements AdapterView.O
                 }
             }
         }
-
          ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list1);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//spinner wiekszy na cały ekran
-       // spinnerAdapter.notifyDataSetChanged();
         spinner.setAdapter(spinnerAdapter);
-
         spinner.setOnItemSelectedListener(this);
     }
-
-
     public void BtnBUY(View view) {
        upDateTextView();
-
         Intent anotherIntent5= new Intent(getApplicationContext(), AccountsActivity.class);
         startActivity(anotherIntent5);
-
-    }
-
+   }
+    //edittext nazwe zmienic na currencyAmount
     private void upDateTextView() {
-        foreignCurs=Double.valueOf((textForeign.getText()).toString());
-        buyAmount = Double.parseDouble(String.valueOf(editText.getText())); //editText.getText
+        foreignCurs = Double.valueOf((textForeign.getText()).toString());
         plnAmount = Double.parseDouble(String.valueOf(textAmount.getText()));
+        buyAmount = Double.parseDouble(String.valueOf(editText.getText())); //editText.getText
+        result =  foreignCurs * buyAmount;
 
-        double result=  foreignCurs * buyAmount;
         double newPlnAmount = plnAmount - result;
         if (result > plnAmount)
         {
             Toast.makeText(this,"You don't have enough money",Toast.LENGTH_SHORT).show();
         }
-            else
+        else
+        {
             mainAccount.setAmount(newPlnAmount);
             textAmount.setText(String.valueOf(mainAccount.getAmount()));
-//            int i = 0;
-//            accountsList.add(new Account(buyAmount, MainActivity.currencyArrayList.get(i)));
+            
+            for (int i = 0; i < accountsList.size(); i++) {
+                Account account = accountsList.get(i);
+
+                if(account.getCurrency().getSymbol().equals(spinner.getSelectedItem().toString())) {
+                    account.setAmount(buyAmount);
+                }
+            }
+        }
     }
-
-
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
         Toast.makeText(parent.getContext(), "You chose this currency: " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
         textForeign.setText(String.valueOf(MainActivity.currencyArrayList.get(position).getBuy()));
         //parent.getItemAtPosition(position).toString();
-        //Toast.makeText(this,"Yyy",Toast.LENGTH_SHORT).show();
     }
-
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
-
     }
 }
 
