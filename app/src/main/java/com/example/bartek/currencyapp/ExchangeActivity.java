a8369bfe16aa79a619e97a93205e23b00d67186a
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-03-07 13:53:13 +0100

    Update and remowe whitespaces
    Update- now is possible buy one-currency a lot of times, and value is
    adding instead of changing

--- app/src/main/java/com/example/bartek/currencyapp/ExchangeActivity.java
+++ app/src/main/java/com/example/bartek/currencyapp/ExchangeActivity.java
@@ -21,40 +21,32 @@ import static com.example.bartek.currencyapp.AccountsActivity.mainAccount;
 
 public class ExchangeActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-
     TextView textAmount;
     TextView textForeign;
     EditText editText;
     Button buyButton;
-    double foreignCurs;
-    double buyAmount ;
-    double plnAmount;
-    TextView otherAmount;
-    double result;
     Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exchange);
 
-        textAmount=(TextView)findViewById(R.id.textAmount1);
-        textForeign=(TextView)findViewById(R.id.textForeign);
-        editText=(EditText) findViewById(R.id.EditSum);
-        buyButton=(Button)findViewById(R.id.BuyButton);
+        textAmount = (TextView)findViewById(R.id.textAmount1);
+        textForeign = (TextView)findViewById(R.id.textForeign);
+        editText = (EditText) findViewById(R.id.EditSum);
+        buyButton = (Button)findViewById(R.id.BuyButton);
         textAmount.setText(mainAccount.getAmount().toString()); //it can be String.valueof()
         textForeign.setText(String.valueOf("Course of currency "));
         addSpinnerList();
-        otherAmount = (TextView) findViewById(R.id.amountOtherCurrency);
-
     }
     private void addSpinnerList() {
         spinner = (Spinner) findViewById(R.id.spinner);
         String[] array_spinner;
         ArrayList<String> list1= new ArrayList();
-        for (int y=0; y < MainActivity.currencyArrayList.size() ; y++)
+        for (int y = 0; y < MainActivity.currencyArrayList.size() ; y++)
         {
-            array_spinner= new String[MainActivity.currencyArrayList.size()];
-            array_spinner[y]=String.valueOf(MainActivity.currencyArrayList.get(y).getSymbol());
+            array_spinner = new String[MainActivity.currencyArrayList.size()];
+            array_spinner[y] = String.valueOf(MainActivity.currencyArrayList.get(y).getSymbol());
             for (String s : array_spinner)
             {
                 if (s != null && s.length() >0)
@@ -63,23 +55,22 @@ public class ExchangeActivity extends AppCompatActivity implements AdapterView.O
                 }
             }
         }
-         ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list1);
+        ArrayAdapter spinnerAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, list1);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);//spinner wiekszy na cały ekran
         spinner.setAdapter(spinnerAdapter);
         spinner.setOnItemSelectedListener(this);
     }
     public void BtnBUY(View view) {
-       upDateTextView();
+        upDateTextView();
         Intent anotherIntent5= new Intent(getApplicationContext(), AccountsActivity.class);
         startActivity(anotherIntent5);
    }
     //edittext nazwe zmienic na currencyAmount
     private void upDateTextView() {
-        foreignCurs = Double.valueOf((textForeign.getText()).toString());
-        plnAmount = Double.parseDouble(String.valueOf(textAmount.getText()));
-        buyAmount = Double.parseDouble(String.valueOf(editText.getText())); //editText.getText
-        result =  foreignCurs * buyAmount;
-
+        double foreignCurs = Double.valueOf((textForeign.getText()).toString());
+        double plnAmount = Double.parseDouble(String.valueOf(textAmount.getText()));
+        double buyAmount = Double.parseDouble(String.valueOf(editText.getText())); //editText.getText
+        double result =  foreignCurs * buyAmount;
         double newPlnAmount = plnAmount - result;
         if (result > plnAmount)
         {
@@ -89,12 +80,13 @@ public class ExchangeActivity extends AppCompatActivity implements AdapterView.O
         {
             mainAccount.setAmount(newPlnAmount);
             textAmount.setText(String.valueOf(mainAccount.getAmount()));
-            
             for (int i = 0; i < accountsList.size(); i++) {
                 Account account = accountsList.get(i);
-
                 if(account.getCurrency().getSymbol().equals(spinner.getSelectedItem().toString())) {
-                    account.setAmount(buyAmount);
+                //dzieki temu kupując jedna walutę kilka razy, wartość się dodaje, a nie nowa zastępuje starą.
+                    double buyAmount2 = account.getAmount();
+                    buyAmount2 += buyAmount;
+                    account.setAmount(buyAmount2);
                 }
             }
         }
@@ -103,7 +95,6 @@ public class ExchangeActivity extends AppCompatActivity implements AdapterView.O
     public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
         Toast.makeText(parent.getContext(), "You chose this currency: " + parent.getItemAtPosition(position).toString(), Toast.LENGTH_LONG).show();
         textForeign.setText(String.valueOf(MainActivity.currencyArrayList.get(position).getBuy()));
-        //parent.getItemAtPosition(position).toString();
     }
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
