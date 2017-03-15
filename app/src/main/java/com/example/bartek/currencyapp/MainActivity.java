22e7989296e605cc54cff8b925106cce6cc88370
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-03-15 15:42:55 +0100

    Remowe white spaces

--- app/src/main/java/com/example/bartek/currencyapp/MainActivity.java
+++ app/src/main/java/com/example/bartek/currencyapp/MainActivity.java
@@ -29,155 +29,82 @@ public class MainActivity extends AppCompatActivity {
     static String TAG = "Main" ;
 
     public static ArrayList<Currency> currencyArrayList= new ArrayList<Currency>();
-
     CurrencyAdapter adapter;
-
     ListView listView;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         listView = (ListView) findViewById(R.id.list);
-
         fillCurrencyList();
-
-
     }
-
-
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             getMenuInflater().inflate(R.menu.menu1, menu);
             return super.onCreateOptionsMenu(menu);
-
-
         }
-
         @Override
         public boolean onOptionsItemSelected(MenuItem item) {
             int id= item.getItemId();
             if (id == R.id.action){
                 openActivity();
-
             }
             return super.onOptionsItemSelected(item);
         }
-
         private void openActivity() {
             Intent intent= new Intent(this, AccountsActivity.class);
-
-
-
             startActivity(intent);
-
         }
-
-
-
-
-
-
-
     private void fillCurrencyList() {
-
-
-
-
         new JSONAsyncTask().execute();
-
         adapter = new CurrencyAdapter(getApplicationContext(), R.layout.row, currencyArrayList);
-
         listView.setAdapter(adapter);
-
-
     }
-
-
-
     class JSONAsyncTask extends AsyncTask<Void, Void, Void> {
-
-
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
             Toast.makeText(MainActivity.this, "Currency is downloading", Toast.LENGTH_LONG).show();
-
         }
-
         @Override
         protected Void doInBackground(Void... voids) {
-
             try {
-
-
                 HttpGet get = new HttpGet("http://api.nbp.pl/api/exchangerates/tables/c");
                 get.setHeader("Accept","application/json");
                 HttpClient httpclient = new DefaultHttpClient();
                 HttpResponse response = httpclient.execute(get);
-
                 int status = response.getStatusLine().getStatusCode();
-
                 if (status == 200) {
                     HttpEntity entity = response.getEntity();
                     String data = EntityUtils.toString(entity);
-
                     try {
                         JSONArray apiData = new JSONArray(data);
-
-
                         JSONObject o = apiData.getJSONObject(0);
-
                         JSONArray jarray = o.getJSONArray("rates");
-
-
-
                         for (int i = 0; i < jarray.length(); i++) {
-
                             JSONObject c = jarray.getJSONObject(i);
-
-
                             Currency oneCurrency = new Currency();
-
-
                             oneCurrency.setSymbol(c.getString("code"));
                             oneCurrency.setBuy (c.getDouble("bid"));
                             oneCurrency.setSale(c.getDouble("ask"));
-
-
-
                             currencyArrayList.add(oneCurrency);
-
-
                         }
-
                     } catch (JSONException ignored) {
                         Log.e(TAG,ignored.getMessage());
                     }
-
                 } else
                 {
                     Log.e(TAG, "status error"+ String.valueOf(status) );
                 }
-
-
-
             } catch (java.io.IOException e) {
                 Log.e(TAG, e.getMessage());
             }
             return null;
-
         }
-
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-
             adapter.notifyDataSetChanged();
-
         }
     }
 }
