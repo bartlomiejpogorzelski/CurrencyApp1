825743c05fd3231196c896126dc5596f5192d56c
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-03-15 15:46:38 +0100

    Remove white spaces and add spacing

--- app/src/main/java/com/example/bartek/currencyapp/CurrencyAdapter.java
+++ app/src/main/java/com/example/bartek/currencyapp/CurrencyAdapter.java
@@ -26,52 +26,30 @@ public class CurrencyAdapter extends ArrayAdapter<Currency> {
         
         vi=(LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        Resource=resource;
-        currencyArrayList=objects;
-
-
+        Resource = resource;
+        currencyArrayList = objects;
     }
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-
         View v = convertView;
-
         if (v == null){
-
-            holder= new HolderView();
-
-            v=vi.inflate(Resource, null);
-
-
-
-            holder.symbol1=(TextView)v.findViewById(R.id.symbol);
-            holder.buy2=(TextView)v.findViewById(R.id.buy);
-            holder.sale3=(TextView)v.findViewById(R.id.sale);
-
-
-
-
+            holder = new HolderView();
+            v = vi.inflate(Resource, null);
+            holder.symbol1 = (TextView)v.findViewById(R.id.symbol);
+            holder.buy2 = (TextView)v.findViewById(R.id.buy);
+            holder.sale3 = (TextView)v.findViewById(R.id.sale);
             v.setTag(holder);
         } else {
             holder = (HolderView) v.getTag();
         }
-
         holder.symbol1.setText(currencyArrayList.get(position).getSymbol());
         holder.buy2.setText(String.valueOf(currencyArrayList.get(position).getBuy()));
         holder.sale3.setText(String.valueOf(currencyArrayList.get(position).getSale()));
-
         return v;
-
     }
-
          static class HolderView{
             public TextView symbol1;
             public TextView buy2;
             public TextView sale3;
-
     }
-
-
 }
