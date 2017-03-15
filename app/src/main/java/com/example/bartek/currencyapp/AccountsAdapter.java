babb349b885a35c4f6f8a5cfcb04a0cd128eea28
bartlomiejpogorzelski <bartlomiej.pogorzelski@onet.pl>
2017-03-15 15:23:47 +0100

    Remove whitespaces

--- app/src/main/java/com/example/bartek/currencyapp/AccountsAdapter.java
+++ app/src/main/java/com/example/bartek/currencyapp/AccountsAdapter.java
@@ -28,73 +28,40 @@ import static com.example.bartek.currencyapp.R.layout.row1;
 
 public class AccountsAdapter extends ArrayAdapter<Account> {
 
-    ArrayList<Account> accounts;
     LayoutInflater vi;
-    int Resource;
-    HolderView holder;
-    Context context;
-
 
     public AccountsAdapter(Context context, int resource, ArrayList<Account> object) {
-
         super(context, resource, object);
-
-
         vi = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
-
     @NonNull
     @Override
     public View getView(final int position, final View convertView, @NonNull final ViewGroup parent) {
-
         View v = convertView;
-
         if (v == null) {
-
             holder = new HolderView();
-
             v = vi.inflate(row1, null);
-
             holder.button1 = (Button) v.findViewById(R.id.buttonSmall);
             holder.buy2 = (TextView) v.findViewById(R.id.buy11);
-
             v.setTag(holder);
         } else {
             holder = (HolderView) v.getTag();
         }
-
         holder.button1.setText(currencyArrayList.get(position).getSymbol());
         holder.buy2.setText(String.valueOf(currencyArrayList.get(position).getBuy()));
-
-//                  Button button1=(Button) v.findViewById(R.id.buttonSmall);
-
 
         holder.button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-//                                Intent exchangeIntent= new Intent(view.getContext(), ExchangeActivity.class);
-//
-//                                context.startActivity(exchangeIntent);
                 Toast.makeText(context, "działa", Toast.LENGTH_SHORT).show();
             }
         });
-
-
-        //v.setTag(holder);
-//             Intent anotherIntent= new Intent(context, ExchangeActivity.class);
-//
-//                context.startActivity(anotherIntent);
-//            }
         return v;
     }
-
-
     static class HolderView {
         public Button button1;
         public TextView buy2;
-
     }
 
 }
